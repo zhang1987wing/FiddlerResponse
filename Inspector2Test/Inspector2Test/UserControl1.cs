b40301435b435fac689a1dd32f916133f372da4c
wing <thisiszy@gmail.com>
2013-05-21 01:01:15 +0800

    17
    Inspector

---
+++ Inspector2Test/Inspector2Test/UserControl1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Text;
+using System.Windows.Forms;
+using System.Xml;
+using System.Web;
+using System.Net;
+
+namespace Inspector2Test
+{
+    public partial class UserControl1 : UserControl
+    {
+        public UserControl1()
+        {
+            InitializeComponent();
+        }
+
+        public void TextDisplay(string str)
+        {
+            if (null != str && "" != str)
+                this.richTextBox.Text = HttpUtility.UrlDecode(str);
+            else
+                this.richTextBox.Text = "";
+        }
+
+        public RichTextBox getRichTextBox()
+        {
+            return this.richTextBox;
+        }
+
+        public static string UrlEncode2(string str, Encoding Encoding)
+        {
+            if (Encoding == null)
+            {
+                return str;
+            }
+            StringBuilder sb = new StringBuilder();
+            byte[] byStr = Encoding.GetBytes(str); ;
+            System.Text.RegularExpressions.Regex regKey = new System.Text.RegularExpressions.Regex("^[A-Za-z0-9]+$");
+            for (int i = 0; i < byStr.Length; i++)
+            {
+                string strBy = Convert.ToChar(byStr[i]).ToString();
+                if (regKey.IsMatch(strBy))
+                {
+                    sb.Append(strBy);
+                }
+                else
+                {
+                    sb.Append(@"%" + Convert.ToString(byStr[i], 16).ToUpper());
+                }
+            }
+            return sb.ToString().Replace("%D%A", "%0A");
+        }
+
+    }
+}
---
+++ Inspector2Test/Inspector2Test/WebViewer.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Fiddler;
+
+[assembly: Fiddler.RequiredVersion("2.3.0.0")]
+
+namespace Inspector2Test
+{
+    public class WebViewer : Inspector2, IResponseInspector2
+    {
+        UserControl1 testControl;
+        private byte[] m_entityBody;
+        HTTPResponseHeaders m_Headers;
+        private bool m_bDirty;
+
+        private bool m_bReadOnly;
+
+        public bool bReadOnly
+        {
+            get
+            {
+                return m_bReadOnly;
+            }
+            set
+            {
+                m_bReadOnly = value;   // TODO: You probably also want to turn your visible control GRAY (false) or WHITE (true) here depending on the value being passed in.   
+            }
+        }
+
+        public HTTPResponseHeaders headers
+        {
+            get
+            {
+                return null;    // Return null if your control doesn't allow header editing.
+            }
+            set
+            {
+                m_Headers = value;
+            }
+        }
+
+        public byte[] body
+        {
+            get
+            {
+                return m_entityBody;
+            }
+            set
+            {
+                m_entityBody = value;
+                string content_type = "";
+                if (m_Headers != null)
+                    content_type = m_Headers["Content-Type"];
+                testControl.TextDisplay(System.Text.Encoding.UTF8.GetString(value)); // TODO: Use correct encoding based on content header?
+                m_bDirty = false;   // Note: Be sure to have an OnTextChanged handler for the textbox which sets m_bDirty to true!
+            }
+        }
+
+        public bool bDirty
+        {
+            get
+            {
+                return m_bDirty;
+            }
+        }
+
+        public override void AddToTab(System.Windows.Forms.TabPage o)
+        {
+            testControl = new UserControl1();
+            o.Text = "TiancityView";
+            o.Controls.Add(testControl);
+            o.Controls[0].Dock = DockStyle.Fill;
+        }
+
+        public override int GetOrder()
+        {
+            return 0;
+        }
+
+        public void Clear()
+        {
+            m_entityBody = null;
+            testControl.getRichTextBox().Clear();
+        }
+    }
+}
